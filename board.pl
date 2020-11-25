4dd45af55574005dbe808493dbe9da93ce705554
Jose Silva <callfodutypsp@gmail.com>
2020-11-25 15:51:29 +0000

    Decent input function

--- board.pl
+++ board.pl
@@ -44,12 +44,59 @@ create_board(X, L) :-
 	_AlmostFinal = [ _RedRow | _MiddleBoard],
 	append(_AlmostFinal, [_YellowRow], X).
 	
+	
+/* ---------- INPUT ---------- */
 
+char_number(0, '0').
+char_number(1, '1').
+char_number(2, '2').
+char_number(3, '3').
+char_number(4, '4').
+char_number(5, '5').
+char_number(6, '6').
+char_number(7, '7').
+char_number(8, '8').
+char_number(9, '9').
+
+match_char(Cur, X, X, [_ | _]) :-
+	Cur == '\n'.
+	
+match_char(Cur, Acc, X, ListRead) :-
+	char_number(Int, Cur),
+	NewVal is Acc*10 + Int,
+	append(ListRead, [Int], NewList),
+	get_user_input_number_int(NewVal, X, NewList).
+	
+match_char(Cur, _, _, _) :-
+	fail.
+
+get_user_input_number_int(Acc, X, ListRead) :-
+	get_char(_Cur),
+	match_char(_Cur, Acc, X, ListRead).
+
+get_user_input_number(X):-
+	repeat,
+	get_user_input_number_int(0, X, []).
+	
+get_user_play(X,Y) :-
+	write('Coords fam'),
+	nl,
+	get_user_input_number(X),
+	get_user_input_number(Y).
+	
+	
+	
 /* ---------- GAME STATES ---------- */
 
+
+	
 play :- 
 	create_board(_X, 7),
-	print_game(_X, 0, 0).
+	print_game(_X, 0, 0),
+	get_user_play(X, Y),
+	write(X),
+	nl,
+	write(Y).
 
 middleBoard([
 					[3, 0, 0, 0, 0, 0, 0],
